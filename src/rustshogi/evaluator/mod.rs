7bfb2369f57fcd92d60b6260bfd3331d17182c10
applyuser160 <syunsuke130121@gamil.com>
2025-11-07 20:12:56 +0900

    move nn_model to evaluator

--- src/rustshogi/evaluator/mod.rs
+++ src/rustshogi/evaluator/mod.rs
@@ -1,8 +1,10 @@
 pub mod abst;
 pub mod database;
 pub mod neural;
+pub mod nn_model;
 pub mod simple;
 
 pub use abst::Evaluator;
 pub use neural::NeuralEvaluator;
+pub use nn_model::NnModel;
 pub use simple::SimpleEvaluator;
